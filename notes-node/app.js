1d4426c7e37e8647e5259aad59c180c9edb0dcd2
Tomasz Solik <tomasz.solik@gmail.com>
2018-09-17 14:53:16 +0200

    Added argument descriptions
    Code clean up
    GetAll functionality

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,24 +1,45 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash')
 const notes = require('./notes.js');
 const yargs = require('yargs');
+const titleOptions = {
+    describe: 'Title of the note',
+    demand: true,
+    alias: 't'
+};
+const bodyOptions = {
+    describe: 'Body of the note',
+    demand: true,
+    alias: 'b'
+}
 
-const argv = yargs.argv;
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
 var command = argv._[0];
-console.log('Command: ', command);
-
 
 if (command === 'add') {
-   var note = notes.addNote(argv.title, argv.body);
-   var message = note ? notes.logNote(note) : `Note with title ${argv.title} already exist!`;
-   console.log(message);
+    var note = notes.addNote(argv.title, argv.body);
+    var message = note ? notes.logNote(note) : `Note with title ${argv.title} already exist!`;
+    console.log(message);
 } else if (command === 'list') {
-    notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach(note => notes.logNote(note));
 } else if (command === 'read') {
     var note = notes.getNote(argv.title);
-    note ? notes.logNote(note) :  console.log("Note doesn't exist");
+    note ? notes.logNote(note) : console.log("Note doesn't exist");
 } else if (command === 'remove') {
     var noteRemoved = notes.removeNote(argv.title);
     var message = noteRemoved ? 'Note was removed' : 'Note not found';
