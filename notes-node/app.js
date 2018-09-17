61fded1a4cbdb8e3ceec4c1abee74282db4cc49f
Tomasz Solik <tomasz.solik@gmail.com>
2018-09-17 12:06:43 +0200

    Added basic functions to notes.js file
    Added yarg

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,12 +1,22 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash')
+const notes = require('./notes').default;
+const yargs = require('yargs');
 
-var user = os.userInfo();
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
-    if (err) {
-        console.log("Unable to write to file");
-    }
-}); 
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not recognized');
+}
