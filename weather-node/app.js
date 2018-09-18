0bc613161a82f0a99997afa54e672d94fd5e4108
Tomasz Solik <tomasz.solik@gmail.com>
2018-09-18 12:57:53 +0200

    Code refactor

--- weather-node/app.js
+++ weather-node/app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -14,19 +14,10 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-console.log(argv);
-
-request({
-    url: `http://www.mapquestapi.com/geocoding/v1/address?key=HP7GQ0j3qnosRqz0s7x1MAhBF3jQqUIb&location=${encodeURIComponent(argv.address)}`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to connect to Google servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find that address.');
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].providedLocation.location}`);
-        console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-        console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
---
+++ weather-node/geocode/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+    request({
+        url: `http://www.mapquestapi.com/geocoding/v1/address?key=HP7GQ0j3qnosRqz0s7x1MAhBF3jQqUIb&location=${encodeURIComponent(address)}`,
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            callback('Unable to connect to Google servers.');
+        } else if (body.status === 'ZERO_RESULTS') {
+            callback('Unable to find that address.');
+        } else if (body.info.statuscode === 0) {
+            callback(undefined, {
+                address:  body.results[0].providedLocation.location,
+                latitude: body.results[0].locations[0].latLng.lat,
+                longitude: body.results[0].locations[0].latLng.lng
+            });            
+        } else {
+            console.log(JSON.stringify(body, undefined, 2));
+        }
+    });
+};
+
+module.exports.geocodeAddress = geocodeAddress;
