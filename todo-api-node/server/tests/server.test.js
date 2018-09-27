d11df269109086aadecf185f468b198e6908e18d
Tomasz Solik <tomasz.solik@gmail.com>
2018-09-27 11:07:41 +0200

    Refactor for tests file

--- todo-api-node/server/tests/server.test.js
+++ todo-api-node/server/tests/server.test.js
@@ -4,22 +4,10 @@ const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed'); 
 
-const todos = [{
-    _id: new ObjectID(),
-    text: 'First Test Todo'
-}, {
-    _id: new ObjectID(),
-    text: 'Second Test Todo',
-    completed: true,
-    completedAt: 333
-}];
-
-beforeEach((done) => {
-    Todo.remove({}).then(() => {
-        return Todo.insertMany(todos);
-    }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
     it('Should create a new todo', (done) => {
